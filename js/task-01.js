02f2d7989539cd152561e8d55ec6acc328715cb7
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-10-04 20:08:24 +0300

    5

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// Number of categorie
+const itemsEl = document.querySelectorAll('.item');
+
+console.log(`Number of categories: `, itemsEl.length);
+
+// Animals
+const animalsEl = document.querySelectorAll('li.item');
+
+console.log(`Category: Animals. 
+Elements: `, animalsEl);
+
+
+
